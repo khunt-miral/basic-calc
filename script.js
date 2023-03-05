84fe8cae0a69bdef260b31f556f7b67883383fe2
Khunt Miral <20ce051.miral.khunt@vvpedulink.ac.in>
2023-03-05 22:32:48 +0530

    comments added

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ let arr = Array.from(buttons);
 
 arr.forEach(button => {
     button.addEventListener('click', (e) => {
+        //if equal button is pressed
         if (e.target.innerHTML == '=') {
             string = eval(string);
             input.value = string;
-        }else if(e.target.innerHTML == 'AC'){
+        }
+        //if all clear button is pressed
+        else if(e.target.innerHTML == 'AC'){
             string = "";
             input.value = string;
         }else if(e.target.innerHTML == 'DEL') {
